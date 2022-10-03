f65acbca8c78e28352da27041350a7b27571b908
Thang Nguyen <89552284+ThangNg98@users.noreply.github.com>
2022-10-02 19:49:32 -0500

    Update models.js

--- backend/models/models.js
+++ backend/models/models.js
@@ -87,6 +87,97 @@ let eventDataSchema = new Schema({
     collection: 'eventData'
 });
 
+let orgDataSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    orgName: {
+        type: String,
+        require: true
+    },
+    
+    primaryData: {
+        _id: { type: String, default: uuid.v1 },
+        firstName: {
+            type: String,
+            require: true
+        },
+        middleName: {
+            type: String,
+        },
+            lastName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String
+        },
+        phoneNumbers: {
+            type: Array,
+            required: true
+        },
+        address: {
+            line1: {
+            type: String
+        },
+            line2: {
+            type: String,
+        },
+            city: {
+            type: String,
+            required: true
+        },
+        county: {
+            type: String,
+        },
+        zip: {
+            type: String,
+        }
+        },
+    },
+
+    eventData: {
+        _id: { type: String, default: uuid.v1 },
+        eventName: {
+            type: String,
+            require: true
+        },
+        services: {
+            type: Array
+        },
+        date: {
+            type: Date,
+            required: true
+        },
+        address: {
+            line1: {
+            type: String
+        },
+            line2: {
+            type: String,
+        },
+            city: {
+            type: String,
+        },
+            county: {
+            type: String,
+        },
+            zip: {
+            type: String,
+        }
+        },
+        description: {
+            type: String,
+        },
+        attendees: [{
+            type: String
+        }]
+
+    },
+  
+}, {
+    collection: 'orgData',
+    timestamps: true
+});
+
 // create models from mongoose schemas
 const primarydata = mongoose.model('primaryData', primaryDataSchema);
 const eventdata = mongoose.model('eventData', eventDataSchema);
