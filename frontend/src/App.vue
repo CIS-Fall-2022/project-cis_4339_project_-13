df20d050e3d9c5b89d47c0c30e2ffa1ee4e8c219
Thang Nguyen <nguyenn.thang@yahoo.com>
2022-11-16 04:59:15 -0600

    finished API call to backend for orgName

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -48,7 +48,7 @@
           background: linear-gradient(250deg, #C8102E 70%, #efecec 50.6%);
         "
       >
-        <h1 class="mr-20 text-3xl text-white">Dataplatform</h1>
+        <h1 class="mr-20 text-3xl text-white"> {{org.orgId}} </h1>
       </section>
       <div>
         <router-view></router-view>
@@ -58,7 +58,23 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+  data() {
+    return {
+      org: '',
+    }
+  },
+  created() {
+            //make call to backend to get orgName
+              let apiURL = 'http://localhost:3000/orgData/org';
+              axios.get(apiURL).then(res => {
+                  this.org = res.data;
+              }).catch(error => {
+                  console.log(error)
+              }); 
+          },
   name: "App",
 };
 </script>
