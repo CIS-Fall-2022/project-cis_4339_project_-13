4df26efa7ac2ce94bcbd659fc6386e123b99524d
dherna44 <elitefc16@gmail.com>
2022-11-20 02:03:10 -0600

    Added BootstrapVue3 to main.js. (Changes were made as comments, as to not make any real changes yet.) Please feel free to make any changes.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import router from './router'
+//import BootstrapVue3 from 'bootstrap-vue-3'
 import App from './App.vue'
 import './index.css'
 
+// -- adding bootstrap to main.js --
+//import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+//import axios from "axios";
+
 const app = createApp(App);
 app.use(router);
+//app.use(BootstrapVue3)
 app.mount('#app');
